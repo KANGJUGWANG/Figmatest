67c5ca7f9d000dfa69f4ac6a01aeab80c6736312
강주광 <55911718+KANGJUGWANG@users.noreply.github.com>
2026-04-06 21:52:05 +0900

    ????

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',  // 이 줄을 추가하세요!
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
